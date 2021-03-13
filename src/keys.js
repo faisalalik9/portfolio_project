1f89aae81409c038cb13a66f89a692727ac8e7be
Faisal <faisalalik9@gmail.com>
2021-03-13 11:37:28 +0530

    some minor changes

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+  googleClientID : '331750677397-u4us2opkqk42j9kklsf35pfgqo7bji0b.apps.googleusercontent.com',
+  googleClientSecret: 'moVSc90g2iJGMrrkJ37RDQPo',
+  cookieKey: 'dsfslslfeekfdkjfdkfdkfjdkdjs'
+};
